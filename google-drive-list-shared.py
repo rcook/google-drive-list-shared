e74fb919684d8787008711e89c4ad8c6f09cb0ab
James W Thorne <jameswthorne@gmail.com>
2018-10-18 20:34:09 -0500

    Updated Python script to support all Google Drive files

--- google-drive-list-shared.py
+++ google-drive-list-shared.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 import time
+import ast
 
 from apiclient import discovery
 from httplib2 import Http
@@ -11,19 +12,37 @@ SCOPES = 'https://www.googleapis.com/auth/drive.readonly.metadata'
 store = file.Storage('storage.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
     creds = tools.run_flow(flow, store)
 
-DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
-file_service = DRIVE.files()
-files = file_service.list().execute().get('files', [])
-for f in files:
-    file_shared = (file_service.get(fileId=f['id'], fields="name, shared").execute())
-    
-    # Only display files that are shared
-    if file_shared['shared']:
-        print(file_shared['name'])
+service = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+results = service.files().list(
+        pageSize=1000,
+        fields="nextPageToken, files(name, shared)").execute()
+token = results.get('nextPageToken', None)
+items = results.get('files', [])
 
-    # Sleep 1/10 of a second between every API call, otherwise
-    # you will exceed the number of calls allowed
-    time.sleep(.100)
+while token is not None:
+    results = service.files().list(
+            pageSize=1000,
+            pageToken=token,
+            fields="nextPageToken, files(name, shared)").execute()
+    # Store the new nextPageToken on each loop iteration
+    token = results.get('nextPageToken', None)
+    # Append the next set of results to the items variable
+    items.extend(results.get('files', []))
+
+# The Google Drive does not return valid JSON because the property
+# names are not enclosed in double quotes, they are enclosed in
+# single quotes. So, use Python AST to convert the string to an
+# iterable list.
+items_dict = ast.literal_eval(str(items))
+
+print("You have", len(items_dict), "files in Google Drive\n")
+print("The following files are shared:\n")
+
+# Iterate through the items list and only show files that have
+# shared set to True.
+for i in range(len(items_dict)):
+    if items_dict[i]['shared']:
+        print(items_dict[i]['name'])
